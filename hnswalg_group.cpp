f9612800bde4926d28834d01b2ded0eed8bcdc3f
dbaranchuk <dmitrybaranchuk@gmail.com>
2018-08-24 14:44:34 +0300

    .

--- hnswalg_group.cpp
+++ hnswalg_group.cpp
@@ -76,8 +76,8 @@ namespace hnswlib {
         std::priority_queue<std::pair<float, idx_t >> topResults;
         std::priority_queue<std::pair<float, idx_t >> candidateSet;
 
-        float dist = group2group_dist(target_node, enterpoint_node);
-        //float dist = fvec_L2sqr(centroids.data() + target_node*d, centroids.data() + enterpoint_node*d, d);
+        //float dist = group2group_dist(target_node, enterpoint_node);
+        float dist = fvec_L2sqr(centroids.data() + target_node*d, centroids.data() + enterpoint_node*d, d);
         topResults.emplace(dist, enterpoint_node);
         candidateSet.emplace(-dist, enterpoint_node);
         massVisited[enterpoint_node] = currentV;
@@ -96,8 +96,8 @@ namespace hnswlib {
             for (idx_t node : grouplinks){
                 if (massVisited[node] != currentV) {
                     massVisited[node] = currentV;
-                    float dist = group2group_dist(target_node, node);
-                    //float dist = fvec_L2sqr(centroids.data() + target_node*d, centroids.data() + node*d, d);
+                    //float dist = group2group_dist(target_node, node);
+                    float dist = fvec_L2sqr(centroids.data() + target_node*d, centroids.data() + node*d, d);
 
                     if (topResults.top().first > dist || topResults.size() < ef) {
                         candidateSet.emplace(-dist, node);
@@ -141,14 +141,14 @@ namespace hnswlib {
                 other_grouplinks.push_back(current_node);
             } else {
                 // finding the "weakest" element to replace it with the new one
-                float d_max = group2group_dist(current_node, node);
-                //float dist = fvec_L2sqr(centroids.data() + current_node*d, centroids.data() + node*d, d);
+                //float d_max = group2group_dist(current_node, node);
+                float d_max = fvec_L2sqr(centroids.data() + current_node*d, centroids.data() + node*d, d);
                 std::priority_queue<std::pair<float, idx_t>> candidates;
                 candidates.emplace(d_max, current_node);
 
                 for (idx_t other_grouplink : other_grouplinks)
-                    candidates.emplace(group2group_dist(other_grouplink, node), other_grouplink);
-                    //candidates.emplace(fvec_L2sqr(centroids.data() + other_grouplink*d, centroids.data() + node*d, d);, other_grouplink);
+                    //candidates.emplace(group2group_dist(other_grouplink, node), other_grouplink);
+                    candidates.emplace(fvec_L2sqr(centroids.data() + other_grouplink*d, centroids.data() + node*d, d);, other_grouplink);
 
                 getNeighborsByHeuristic(candidates, maxM);
 
@@ -188,8 +188,8 @@ namespace hnswlib {
             resultSet.pop();
             bool good = true;
             for (std::pair<float, idx_t> current2 : returnlist) {
-                float curdist = group2group_dist(current2.second, current.second);
-                //float curdist = fvec_L2sqr(centroids.data() + current2.second*d, centroids.data() + current.second*d, d);
+                //float curdist = group2group_dist(current2.second, current.second);
+                float curdist = fvec_L2sqr(centroids.data() + current2.second*d, centroids.data() + current.second*d, d);
                 if (curdist < dist_to_query) {
                     good = false;
                     break;
