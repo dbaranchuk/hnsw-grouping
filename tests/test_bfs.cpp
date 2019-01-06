680c8bfc3cb47a0970d091550374805119adb3ed
Dmitry <dmitrybaranchuk@gmail.com>
2019-01-06 22:30:08 +0300

    update

--- tests/test_bfs.cpp
+++ tests/test_bfs.cpp
@@ -10,11 +10,22 @@ static void test_approx(float *massQ, size_t nq,
 {
     StopW stopw =  StopW();
     std::vector<std::vector<idx_t>> results(nq);
-    for (int i = 0; i < 100; i++) {
+
+    #pragma omp parallel for
+    for (int i = 0; i < nq; i++) {
         idx_t enterpoint = quantizer->get_enterpoint(massQ + d*i);
         results[i] = quantizer->bfs(enterpoint, answers[i], 2);
     }
-    std::cout << "TIme(s): " << stopw.getElapsedTimeMicro() * 1e-6 / 10 << std::endl;
+    std::cout << "TIme(s): " << stopw.getElapsedTimeMicro() * 1e-6 / 100 << std::endl;
+
+
+    std::ofstream out("dist_cache_margin2.ivecs", std::ios::binary);
+
+    for (auto result : results){
+        uint32_t dim = result.size();
+        out.write((char *) &dim, sizeof(uint32_t));
+        out.write((char *) result.data(), dim * sizeof(uint32_t));
+    }
 }
 
 
